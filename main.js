dfcf9bcd8435e02784ac01ed7ee701c56193ee45
Lucianazavattaro93 <lucianazavattaro76@gmail.com>
2020-07-30 12:03:18 -0300

    commit

--- main.js
+++ main.js
@@ -6,3 +6,11 @@ document.querySelector(".menu-btn").addEventListener("click", () => {
   ScrollReveal().reveal('.news-cards', { delay: 500 });
   ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
   ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
+
+  var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+  if (!isChrome){
+      $('#iframeAudio').remove()
+  }
+  else {
+      $('#playAudio').remove() // just to make sure that it will not have 2x audio in the background 
+  }
